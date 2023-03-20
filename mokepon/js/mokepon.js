e84da3a8c5b310d345bd6c66ede4ae832a91fc44
Daniel Barrios <gersonbarrios97@gmail.com>
2023-03-20 16:30:42 -0600

    Se cambio al head el link de archivos js, y se agrego código para elegir el mokepon de los inputs radio

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,8 +1,24 @@
 const d = document;
 
-function seleccionarMokeponJugador() {
-  alert('Seleccionaste Mokepon');
+function iniciarJuego() {
+  let $btnSeleccionar = d.getElementById("btn-seleccionar");
+  $btnSeleccionar.addEventListener("click", seleccionarMokeponJugador);
 }
 
-let $btnSeleccionar = d.getElementById('btn-seleccionar');
-$btnSeleccionar.addEventListener('click', seleccionarMokeponJugador);
+function seleccionarMokeponJugador() {
+  let $btnHipodoge = d.getElementById('hipodoge');
+  let $btnCapipepo = d.getElementById('capipepo');
+  let $btnRatigueya = d.getElementById('ratigueya');
+
+  if($btnHipodoge.checked) {
+    alert("Elegiste a Hipodoge");
+  } else if ($btnCapipepo.checked) {
+    alert("Elegiste a Capipepo");
+  } else if ($btnRatigueya.checked) {
+    alert("Elegiste a Ratigueya");
+  } else {
+    alert("Selecciona un Mokepon");
+  }
+}
+
+window.addEventListener("load", iniciarJuego);
